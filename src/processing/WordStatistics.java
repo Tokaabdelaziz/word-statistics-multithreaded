bd16efd7ff18658151f4fd6433dc694dbf37b6d0
SalmaKamal <salmakamal23062005@gmail.com>
2025-12-01 15:01:51 +0200

    Update WordStatistics.java

--- src/processing/WordStatistics.java
+++ src/processing/WordStatistics.java
@@ -1,1 +1,15 @@
+package processing;
 
+public class WordStatistics {
+    public String fileName;
+    public int wordCount;
+    public int countIs;
+    public int countAre;
+    public int countYou;
+    public String longestWord = "";
+    public String shortestWord = "";
+
+    public WordStatistics(String fileName) {
+        this.fileName = fileName;
+    }
+}
