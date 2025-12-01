1323c60d3a9dc2a60eac5bb32ac95be75154c7c3
SalmaKamal <salmakamal23062005@gmail.com>
2025-12-01 15:04:06 +0200

    Update FileProcessingTask.java

--- src/processing/tasks/FileProcessingTask.java
+++ src/processing/tasks/FileProcessingTask.java
@@ -1,1 +1,32 @@
+package processing.tasks;
+
+import processing.*;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileProcessingTask implements Runnable {
+
+    private List<Path> files;
+    private UpdateDispatcher dispatcher; // Person 5 implements this
+
+    public FileProcessingTask(List<Path> files, UpdateDispatcher dispatcher) {
+        this.files = files;
+        this.dispatcher = dispatcher;
+    }
+
+    @Override
+    public void run() {
+
+        FileProcessor processor = new FileProcessor();
+
+        for (Path file : files) {
+
+            WordStatistics stats = processor.process(file);
+
+            dispatcher.sendUpdate(stats);
+        }
+    }
+}
+
 
