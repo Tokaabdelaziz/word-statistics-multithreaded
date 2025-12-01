4dcb4d4ae82228a069ee8aec2527d2630ae7c45d
SalmaKamal <salmakamal23062005@gmail.com>
2025-12-01 15:03:16 +0200

    Update KeywordsCounter.java

--- src/processing/KeywordsCounter.java
+++ src/processing/KeywordsCounter.java
@@ -1,1 +1,13 @@
+package processing;
+
+public class KeywordsCounter {
+
+    public void updateCounts(String word, WordStatistics stats) {
+        String w = word.toLowerCase();
+
+        if (w.equals("is")) stats.countIs++;
+        if (w.equals("are")) stats.countAre++;
+        if (w.equals("you")) stats.countYou++;
+    }
+}
 
