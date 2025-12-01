ccaec8b57d1802398b5bcc2b898a0e5c1f3705fe
SalmaKamal <salmakamal23062005@gmail.com>
2025-12-01 15:02:36 +0200

    Update WordAnalyzer.java

--- src/processing/WordAnalyzer.java
+++ src/processing/WordAnalyzer.java
@@ -1,1 +1,18 @@
 
+package processing;
+
+public class WordAnalyzer {
+
+    public void updateWordLengths(String word, WordStatistics stats) {
+
+        if (word.length() == 0) return;
+
+        if (stats.longestWord.equals("") || word.length() > stats.longestWord.length()) {
+            stats.longestWord = word;
+        }
+
+        if (stats.shortestWord.equals("") || word.length() < stats.shortestWord.length()) {
+            stats.shortestWord = word;
+        }
+    }
+}
