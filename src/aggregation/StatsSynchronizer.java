a8e4eeea912a476ca1b47a09980aa4f50a5b754f
mariamsaeed99 <166349529+mariamsaeed99@users.noreply.github.com>
2025-12-02 03:49:46 +0200

    Update StatsSynchronizer.java

--- src/aggregation/StatsSynchronizer.java
+++ src/aggregation/StatsSynchronizer.java
@@ -1,1 +1,19 @@
+package aggregation;
+
+// Ensures thread-safe updates
+public class StatsSynchronizer {
+    private DirectoryStatistics directoryStats;
+
+    public StatsSynchronizer() {
+        directoryStats = new DirectoryStatistics();
+    }
+
+    public synchronized void update(FileStatistics fileStats) {
+        directoryStats.updateStats(fileStats);
+    }
+
+    public DirectoryStatistics getDirectoryStatistics() {
+        return directoryStats;
+    }
+}
 
