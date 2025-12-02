e0962ff627326aa6391b36b38f576e1d61abddab
malkhussam84-dotcom <malkhussam84@gmail.com>
2025-12-02 15:06:44 +0200

    Update ScanResult.java

--- src/filesystem/ScanResult.java
+++ src/filesystem/ScanResult.java
@@ -1,1 +1,27 @@
+package filesystem;
+
+import java.util.List;
+
+public class ScanResult {
+
+    private List<String> filePaths;
+    private boolean includedSubfolders;
+
+    public ScanResult(List<String> filePaths, boolean includedSubfolders) {
+        this.filePaths = filePaths;
+        this.includedSubfolders = includedSubfolders;
+    }
+
+    public List<String> getFilePaths() {
+        return filePaths;
+    }
+
+    public int getFileCount() {
+        return filePaths.size();
+    }
+
+    public boolean includedSubfolders() {
+        return includedSubfolders;
+    }
+}
 
