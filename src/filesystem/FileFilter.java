4a83d5952904f2917555e90467f8d2722d3d6a65
malkhussam84-dotcom <malkhussam84@gmail.com>
2025-12-02 15:06:22 +0200

    Update FileFilter.java

--- src/filesystem/FileFilter.java
+++ src/filesystem/FileFilter.java
@@ -1,1 +1,11 @@
 
+package filesystem;
+
+import java.io.File;
+
+public class FileFilter {
+
+    public static boolean isTxtFile(File f) {
+        return f.isFile() && f.getName().toLowerCase().endsWith(".txt");
+    }
+}
