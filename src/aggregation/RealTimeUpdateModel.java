23ed9e125b49380f83cc0b47bb68a3a25338bf62
Tokaabdelaziz2 <168280364+Tokaabdelaziz@users.noreply.github.com>
2025-12-02 20:45:43 +0200

    Refactor RealTimeUpdateExample to RealTimeUpdateModel

--- src/aggregation/RealTimeUpdateModel.java
+++ src/aggregation/RealTimeUpdateModel.java
@@ -1,43 +1,27 @@
 package aggregation;
 
-public class RealTimeUpdateExample {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
-       
-        RealTimeUpdateModel model = new RealTimeUpdateModel();
+public class RealTimeUpdateModel {
+    private List<DirectoryStatsListener> listeners = new ArrayList<>();
 
-        
-        RealTimeUpdateModel.DirectoryStatsListener consoleListener = stats -> {
-            System.out.println("=== Stats Updated ===");
-            System.out.println("Total Words: " + stats.getTotalWords());
-            System.out.println("Total 'is': " + stats.getTotalIs());
-            System.out.println("Longest Word: " + stats.getLongestWord());
-            System.out.println("-------------------");
-        };
+    public void addListener(DirectoryStatsListener listener) {
+        listeners.add(listener);
+    }
 
-        
-        model.addListener(consoleListener);
+    public void removeListener(DirectoryStatsListener listener) {
+        listeners.remove(listener);
+    }
 
-      
-        DirectoryStatistics stats1 = new DirectoryStatistics();
-        stats1.updateStats(new FileStatistics(50, 2, 1, 3, "programming", "a"));
+    public void updateStats(DirectoryStatistics stats) {
+        for (DirectoryStatsListener listener : listeners) {
+            listener.onStatsUpdated(stats);
+        }
+    }
 
-        DirectoryStatistics stats2 = new DirectoryStatistics();
-        stats2.updateStats(new FileStatistics(100, 5, 3, 2, "exceptionally", "it"));
-
-        
-        model.updateStats(stats1);
-        model.updateStats(stats2);
-
-    
-        model.removeListener(consoleListener);
-
-      
-        DirectoryStatistics stats3 = new DirectoryStatistics();
-        stats3.updateStats(new FileStatistics(70, 1, 2, 1, "aggregator", "I"));
-        model.updateStats(stats3);
-
-        System.out.println("Example finished.");
+    public interface DirectoryStatsListener {
+        void onStatsUpdated(DirectoryStatistics stats);
     }
 }
 
